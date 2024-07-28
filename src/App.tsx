50d2c75af5f1b4f449aa6a5815cd5d602a38faf5
awqsomee <awqsome@bk.ru>
2024-07-28 11:58:39 +0300

    move input to other file

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,19 @@
 import './App.css'
-import { Input } from './shared/ui/Input'
 import { BytemList } from './components/BytemList'
 import { useUnit } from 'effector-react'
-import {
-  $bytemImportance,
-  $bytemsSorted,
-  $bytemTitle,
-  $showCompleted,
-  appStarted,
-  createBytem,
-  setBytemImportance,
-  setBytemTitle,
-  toggleShowCompleted,
-} from './shared/model'
-import { importance } from './shared/types'
-import { HintList } from './components/HintList'
-import { useEffect, useRef } from 'react'
+import { $showCompleted, appStarted, toggleShowCompleted } from './shared/model'
+import { useEffect } from 'react'
+import { BytemAdder } from './components/BytemAdder'
 
 function App() {
-  const [title, setTitle] = useUnit([$bytemTitle, setBytemTitle])
   const [showCompleted, setShowCompleted] = useUnit([
     $showCompleted,
     toggleShowCompleted,
   ])
-  const [chosenImportance, setImportance] = useUnit([
-    $bytemImportance,
-    setBytemImportance,
-  ])
-  const bytems = useUnit($bytemsSorted)
 
   useEffect(() => {
     appStarted()
   }, [])
-
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
-    if (bytems.some((bytem) => bytem.title === title)) {
-      // TODO: highlight bytem with title
-      return
-    }
-    createBytem({ title, importance: chosenImportance })
-    titleInputRef.current?.focus()
-  }
-
-  const titleInputRef = useRef<HTMLInputElement>(null)
 
   return (
     <div
@@ -67,48 +37,7 @@ function App() {
         </div>
         <BytemList />
       </div>
-      <div
-        style={{
-          textAlign: 'left',
-          position: 'absolute',
-          width: 'clamp(400px, 100%, 900px)',
-          bottom: '0',
-        }}
-      >
-        <HintList />
-        <form
-          style={{
-            display: 'flex',
-            gap: '1rem',
-            alignItems: 'end',
-            marginTop: '1rem',
-          }}
-          onSubmit={handleAdd}
-        >
-          <Input
-            forwardedRef={titleInputRef}
-            type="search"
-            placeholder="Добавить"
-            value={title}
-            setValue={setTitle}
-            style={{ width: '60%', height: '100%', padding: '0.5rem' }}
-          />
-          <select
-            value={chosenImportance}
-            onChange={(e) => setImportance(Number(e.target.value))}
-            style={{ width: '40%', height: '100%', padding: '0.5rem' }}
-          >
-            {importance.map(([key, value]) => (
-              <option key={key} value={key}>
-                {value}
-              </option>
-            ))}
-          </select>
-          <button style={{ maxHeight: '100%' }} type="submit">
-            Add
-          </button>
-        </form>
-      </div>
+      <BytemAdder />
     </div>
   )
 }
---
+++ src/components/BytemAdder.tsx
@@ -0,0 +1,90 @@
+import { HintList } from './HintList'
+import { Input } from '../shared/ui/Input'
+import { useRef } from 'react'
+import { useUnit } from 'effector-react'
+import {
+  $bytemImportance,
+  $bytemsSorted,
+  $bytemTitle,
+  createBytem,
+  setBytemImportance,
+  setBytemTitle,
+} from '../shared/model'
+import { importance } from '../shared/types'
+
+export const BytemAdder = () => {
+  const palceholder = useRef(generatePlaceholder())
+
+  const titleInputRef = useRef<HTMLInputElement>(null)
+  const [title, setTitle] = useUnit([$bytemTitle, setBytemTitle])
+  const bytems = useUnit($bytemsSorted)
+  const [chosenImportance, setImportance] = useUnit([
+    $bytemImportance,
+    setBytemImportance,
+  ])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (bytems.some((bytem) => bytem.title === title)) {
+      // TODO: highlight bytem with title
+      return
+    }
+    createBytem({ title, importance: chosenImportance })
+    titleInputRef.current?.focus()
+  }
+  return (
+    <div
+      style={{
+        textAlign: 'left',
+        position: 'absolute',
+        width: 'clamp(400px, 100%, 900px)',
+        bottom: '0',
+      }}
+    >
+      <HintList />
+      <form
+        style={{
+          display: 'flex',
+          gap: '1rem',
+          alignItems: 'end',
+          marginTop: '1rem',
+        }}
+        onSubmit={handleAdd}
+      >
+        <Input
+          forwardedRef={titleInputRef}
+          type="search"
+          placeholder={palceholder.current}
+          value={title}
+          setValue={setTitle}
+          style={{ width: '60%', height: '100%', padding: '0.5rem' }}
+        />
+        <select
+          value={chosenImportance}
+          onChange={(e) => setImportance(Number(e.target.value))}
+          style={{ width: '40%', height: '100%', padding: '0.5rem' }}
+        >
+          {importance.map(([key, value]) => (
+            <option key={key} value={key}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <button style={{ maxHeight: '100%' }} type="submit">
+          Add
+        </button>
+      </form>
+    </div>
+  )
+}
+
+const generatePlaceholder = () => {
+  const placeholders = [
+    'Велосипед 12000₽ х2 by Awh',
+    'Ноутбук 80000₽ by Awh',
+    'Телевизор 20000₽ by Awh',
+    'Киянка by nerpyshka',
+  ]
+
+  return placeholders[Math.floor(Math.random() * placeholders.length)]
+}
